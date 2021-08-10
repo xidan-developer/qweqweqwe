9cf3e27961b9638b310b73c610acfd2ea870ee8d
xidan-developer <64917329+xidan-developer@users.noreply.github.com>
2021-08-10 16:02:51 +0700

    Update Todo.vue

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,7 +1,20 @@
 <template>
-  <div class="todo">
-    <aside/>
-    <p>Компонент всех списков</p>
+  <div class="container obshii">
+      <div class="row qweqweqwe">
+        <div class="listdel col-md-2">
+          <div>
+            <div><a href="">список 1</a></div>
+            <div><a href="">список 2</a></div>
+          </div>
+          <div>
+            <div><a href="">Поле ввода названия</a></div>
+            <div><a href="">Добаввить список</a></div>
+          </div>
+        </div>
+        <div class="delo col-md-10">
+
+        </div>
+      </div>
   </div>
 </template>
 
@@ -13,5 +26,20 @@ export default {
 </script>
 
 <style scoped lang="scss">
-
+.listdel,delo{
+border: 1px solid black;
+}
+.obshii{
+  height: 500px;
+}
+.qweqweqwe{
+  height: 100%;
+}
+.listdel{
+display: flex;
+flex-direction: column;
+justify-content: space-between;
+align-items: center;
+  text-align: center;
+}
 </style>
